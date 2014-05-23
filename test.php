47238f1985e305f0fa0b2ef6b9c42bb6d4b97b50
rajakannan <rajacse10@gmail.com>
2014-05-23 19:52:00 +0530

    auto update

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ Description: This plugins is used to test the auto update via github
 Plugin URI: http://rajakannan.com
 Author: Raja Kannan
 Author URI: http://rajakannan.com
-Version: 0.1
+Version: 0.2
 License: GPL2
 Text Domain: /language
 */
